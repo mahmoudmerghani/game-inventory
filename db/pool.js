403ce20eda41a0e92f7eedf7acfcd2ff4713c4f6
Mahmoud Mirghani <mahmoudtawfiq777@gmail.com>
2025-12-03 20:48:40 +0200

    Change environment variables

--- db/pool.js
+++ db/pool.js
@@ -3,11 +3,7 @@ import { config } from "dotenv";
 config();
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DB_CONNECTION_URL
 });
 
 export default pool;
